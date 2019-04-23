6ed3cef40b7fdbdc3b943843b7290c87080d6014
Varsha Iyer <varsha.b.iyer@gmail.com>
2019-04-23 15:06:52 -0400

    practice

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Map, GoogleApiWrapper, InfoWindow, Marker} from 'google-maps-react';
+
 
 const mapStyles = {
   width: '100%',
@@ -6,15 +8,48 @@ const mapStyles = {
 };
 
 export class MapContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        showingInfoWindow: false, //Hides or the shows the infoWindow
+        activeMarker: {}, //Shows the active marker upon click
+        selectedPlace: {} //Shows the infoWindow to the selected place upon a marker
+    }; 
+} 
+
+onMarkerClick = (props, marker, e) =>
+  this.setState({
+    selectedPlace: props,
+    activeMarker: marker,
+    showingInfoWindow: true
+});
   render() {
     return (
       <div>
         <h1>Hello Google Maps</h1>
+        <Map 
+    google={this.props.google}
+    initialCenter={{
+      lat: 43.7044, 
+      lng: 43.7044, 
+    }}
+    zoom={16}
+    > 
+    <Marker
+    name={'This is a marker we just made yay!'}
+    position={{lat: 43.703262, lng: -72.288524}}
+    onClick={this.onMarkerClick}
+    />
+    <InfoWindow
+    marker={this.state.activeMarker}
+    visible={this.state.showingInfoWindow}>
+    </InfoWindow>
+    </Map >
       </div>
     ) 
   }
 }
 
 export default GoogleApiWrapper({
-  apiKey: 'ENTER_YOUR_API_KEY_HERE'
+  apiKey: 'AIzaSyCkpmbHVEBfXdk6tfii_k5WyYVI4LJFW8k'
 })(MapContainer);
